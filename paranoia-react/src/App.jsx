178569bb335730bad38d17d2515abeefc78c5d24
ceo7913 <ceo7913@naver.com>
2023-12-02 17:44:10 +0900

    docs: image file add

--- paranoia-react/src/App.jsx
+++ paranoia-react/src/App.jsx
@@ -1,9 +1,13 @@
 import { Cnavas } from './Cnavas';
+import { GameOver } from './pages/GameOver';
+import { Prolog } from './pages/Prolog';
 
 function App() {
   return (
     <div className="App">
       <Cnavas/>
+      {/* <GameOver/> */}
+      {/* <Prolog/> */}
     </div>
   );
 }
